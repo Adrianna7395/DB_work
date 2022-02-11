b12bd7ea36e998292c80a6ebd45c326c8aa89e0e
Adrianna7395 <crystalnoir1010@gmail.com>
2022-02-11 10:54:46 +0000

    update

--- 02 SQL Tables/02c_db_table_SQL.sql
+++ 02 SQL Tables/02c_db_table_SQL.sql
@@ -18,7 +18,7 @@ CREATE DATABASE IF NOT EXISTS boo;
 # Tabelle anzeigen, falls noch nicht vorhanden
 CREATE TABLE IF NOT EXISTS boo.test
 (
-	name VARCHAR(20) NOT NULL DEFAULT "TBA",
+	name VARCHAR(20) NOT NULL UNIQUE DEFAULT "TBA",
 	age INT NOT NULL DEFAULT 0
 
 
@@ -35,6 +35,9 @@ DESCRIBE boo.test;
 INSERT INTO boo.test(name,age) VALUES ("Grizabella",29);
 INSERT INTO boo.test(age,name) VALUES (35,"Alonzo");
 INSERT INTO boo.test VALUES ();
+
+# Doppelte Daten werden zugelassen
+INSERT INTO boo.test(age,name) VALUES (35,"Alonzo, der Coole!");
 
 # Inhalte der Tabelle anzeigen 
 SELECT * FROM boo.test;
